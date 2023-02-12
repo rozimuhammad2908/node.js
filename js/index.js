736470def12460cc09694f3228545cb7a776aa60
rozimuhammad2908 <123812566+rozimuhammad2908@users.noreply.github.com>
2023-02-12 16:25:41 +0500

    node.js

---
+++ js/index.js
@@ -0,0 +1,14 @@
+function parament(y, z) {
+  let a = y;
+  let b = z;
+
+  console.log("a = ", z);
+  console.log("b = ", y);
+
+  console.log("Swapping............");
+
+  console.log("a = " + a);
+  console.log("b = " + b);
+}
+
+parament(3, 2);
